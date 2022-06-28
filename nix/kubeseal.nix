b39fc95a0dde74d278bda6c63896068fc6272bba
Michael <michael.loukeris@icloud.com>
2022-06-28 16:21:57 +0300

    Update kubeseal.nix

--- nix/kubeseal.nix
+++ nix/kubeseal.nix
@@ -1,18 +1,23 @@
-#
 # Custom Sealed Secrets package.
-#
-# Couldn't find any easy way to extend/override Nixpkgs 20.11's own
-# kubeseal expression to make it build kubeseal 0.17.5
+# Actually it's a fat C&P from Nixpkgs:
 #
 # - https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/networking/cluster/kubeseal/default.nix
 #
-# So I've rolled out a bare build recipe whipped together by looking
-# at what the kubeseal Makefile target does. To be replaced with the
-# official Nixpkgs expression when it gets updated to build 0.17.5.
+# The only thing I changed is the `version` attribute below.
+# Uh? Wot?! Why not just use an expression like
 #
-{ lib, stdenv, go_1_17, fetchFromGitHub }:
+#   kubeseal = pkgs.kubeseal.overrideAttrs ( old: rec {
+#     version = "0.17.3";
+#     ...
+#   });
+#
+# Cuz it don't flippin work? If you try that, you actually get whatever
+# version is in your Nixpkgs---`0.16.0` in my case. See my rant about
+# overriding Go packages in `opa.nix`.
+#
+{ lib, buildGoModule, fetchFromGitHub }:
 
-stdenv.mkDerivation rec {
+buildGoModule rec {
   pname = "kubeseal";
   version = "0.17.3";
 
@@ -20,31 +25,21 @@ stdenv.mkDerivation rec {
     owner = "bitnami-labs";
     repo = "sealed-secrets";
     rev = "v${version}";
-    sha256 = "sha256-cqOSMAagefKQiKKtgVbk1UFKYGBXQleJ1pgcJ/VyOnM=";
+    sha256 = "sha256-7u7lsMeeZOUGn8eb8sjV9Td+XNEUPDvbSaITdp1JTf4=";
   };
 
-  GO_LD_FLAGS = "-s -w -X main.VERSION=${version}";
+  vendorSha256 = null;
 
-  buildInputs = [ go_1_17 ];
+  doCheck = false;
 
-  buildPhase = ''
-    mkdir -p $out/mod-cache
-    export GOMODCACHE=$out/mod-cache
+  subPackages = [ "cmd/kubeseal" ];
 
-    mkdir -p $out/build-cache
-    export GOCACHE=$out/build-cache
-
-    go build -o kubeseal -ldflags "$GO_LD_FLAGS" ./cmd/kubeseal
-  '';
-  installPhase = ''
-    mkdir -p $out/bin
-    cp kubeseal $out/bin
-  '';
+  ldflags = [ "-s" "-w" "-X main.VERSION=${version}" ];
 
   meta = with lib; {
     description = "A Kubernetes controller and tool for one-way encrypted Secrets";
     homepage = "https://github.com/bitnami-labs/sealed-secrets";
     license = licenses.asl20;
-    maintainers = with maintainers; [ c0c0n3 ];
+    maintainers = with maintainers; [ groodt ];
   };
 }
